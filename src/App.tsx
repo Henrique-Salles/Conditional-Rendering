1ef00cf81a2b8a93692f6baa59739f61a3bb6efb
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-13 18:51:31 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,7 @@ import "./App.css";
 function App() {
   const [signed, setSigned] = useState(false);
 
-  return (
-    <div>
-      {signed ? <h1>Bem vindo, User123</h1> : <h1>Conecte-se!</h1>}
-      <br />
-      <button onClick={() => setSigned(true)}>Entrar</button>
-      <br /> <br />
-      {signed && (
-        <div>
-          <h1>Deseja sair?</h1>
-          <button onClick={() => setSigned(false)}>Sair</button>
-        </div>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
