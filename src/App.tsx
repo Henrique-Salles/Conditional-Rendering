d63bd996306a5ca365517b070dae5945265af139
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-13 19:33:05 -0300

    Revert "First Commit"
    This reverts commit 1ef00cf81a2b8a93692f6baa59739f61a3bb6efb.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ import "./App.css";
 function App() {
   const [signed, setSigned] = useState(false);
 
-  return <div></div>;
+  return (
+    <div>
+      {signed ? <h1>Bem vindo, User123</h1> : <h1>Conecte-se!</h1>}
+      <br />
+      <button onClick={() => setSigned(true)}>Entrar</button>
+      <br /> <br />
+      {signed && (
+        <div>
+          <h1>Deseja sair?</h1>
+          <button onClick={() => setSigned(false)}>Sair</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
